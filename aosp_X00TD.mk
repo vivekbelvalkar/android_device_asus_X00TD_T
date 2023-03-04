1106102124ccd2c520895ebf39e85020fe00cd17
Vivek Belvalkar <vvkbel@gmail.com>
2023-03-04 15:14:35 +0100

    X00TD : Initial aosp bringup

---
+++ aosp_X00TD.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_X00TD
+PRODUCT_DEVICE := X00TD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := asus
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X00T \
+    PRODUCT_NAME=WW_X00T \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys"
